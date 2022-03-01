0afcd5b79aae9c0354482a151754dc4527bfb7f0
Eyrie48 <eddiie6@hotmail.com>
2022-03-01 14:05:51 -0800

    m

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::get('/calendar', function () {
     $jsonString = file_get_contents(base_path('my_data.json'));
 
     $data = json_decode($jsonString, true);
+    dd(__('title'));
     return view('calendar');
 });
 
