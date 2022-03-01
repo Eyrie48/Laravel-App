f60b6e25827fe9d32e019c63591657538e6a9e4d
Eyrie48 <eddiie6@hotmail.com>
2022-02-28 16:30:48 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/todos', function () {
+    return view('welcome');
+});
+
+Route::get('/calendar', function () {
+    return view('welcome');
+});
+
+Route::get('/board', function () {
+    return view('welcome');
+});
