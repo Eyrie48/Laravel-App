d6aeb3ea0bf0ab39c8807aec871747a6360b3607
Eyrie48 <eddiie6@hotmail.com>
2022-02-28 18:04:41 -0800

    .

---
+++ resources/views/fallback.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'To Do List')
+
+@section('content_header')
+    <h1>Error</h1>
+@stop
+
+@section('content')
+    <div>
+        <h1>Error</h1>
+        <p>Error 404 could not figure out what you were doing.</p>
+        <link rel="stylesheet" href="/css/admin_custom.css">
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('welcome');
-});
-
 Route::get('/todos', function () {
     return view('todos');
 });
