e09532ea08175d00d4d0b7a5897fe36b6ece2133
Eyrie48 <eddiie6@hotmail.com>
2022-03-02 17:43:12 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,13 @@ Route::get('/board', function () {
     return view('board');
 });
 
+Route::get('/events-feed', function () {
+    $jsonString = file_get_contents(base_path('my_data.json'));
+
+    $data = json_encode($jsonString);
+    echo $data;
+    return $data;
+});
 /*
 Route::get('/event-feed', function () {
     return view('event-feed');
