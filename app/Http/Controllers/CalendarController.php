ec354b467bf43e0aea3141ac7aadafee0a55cd29
Eyrie48 <eddiie6@hotmail.com>
2022-03-10 23:37:26 -0800

    Update CalendarController.php

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -14,7 +14,7 @@ class CalendarController extends Controller
     public function index()
     {
         //
-        return view('calendars', compact('calendar'));
+        return view('calendars', compact('calendars'));
     }
 
     /**
