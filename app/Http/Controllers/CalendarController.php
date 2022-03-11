d17b290da7797fca7a65f77bf244f6bef1663f3c
Eyrie48 <eddiie6@hotmail.com>
2022-03-11 13:27:28 -0800

    Update CalendarController.php

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -68,7 +68,7 @@ class CalendarController extends Controller
         
         //$calendar = Calendar::find($id);
         $calendar = Calendar::select('title', 'start_at AS start', 'end_at AS end')->get();
-        return view('calendar.show_cal', compact('calendar'));
+        return view('show_cal', compact('calendar'));
     }
 
     /**
